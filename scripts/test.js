1f6c1b8840708a28d3c41361661a58f35f7e5b34
Michael Mok <pmmmwh@gmail.com>
2019-08-27 00:20:46 +0800

    feat(testing): add test runner script for local and ci

---
+++ scripts/test.js
@@ -0,0 +1,26 @@
+// This script is bootstrapped from the CRA repo and trimmed down to fit library use cases
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'test';
+process.env.NODE_ENV = 'test';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+const jest = require('jest');
+let argv = process.argv.slice(2);
+
+// Watch unless on CI or explicitly running all tests
+if (
+  !process.env.CI &&
+  argv.indexOf('--watchAll') === -1 &&
+  argv.indexOf('--watchAll=false') === -1
+) {
+  argv.push('--watch');
+}
+
+jest.run(argv);
