eeb3c7cdf61f32d9165b85c4565962a348b03abf
Michael Mok <pmmmwh@gmail.com>
2019-08-27 00:20:10 +0800

    chore(testing): setup jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['src/**/*.{js,ts,tsx}', '!src/**/*.d.ts'],
+  setupFilesAfterEnv: [],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,ts,tsx}',
+  ],
+  testEnvironment: 'jest-environment-jsdom-fourteen',
+  transform: {
+    '^.+\\.(js|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|ts|tsx)$'],
+  modulePaths: [],
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+};
