53b20033db3bafb805b9a57f48bdb4232464fc01
Michael Mok <pmmmwh@gmail.com>
2019-08-27 00:57:22 +0800

    feat(polyglot): add root export for publish

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as I18n, I18nProps } from './I18n';
+export { I18nContextProps } from './i18nContext';
+export { default as useLocale } from './useLocale';
+export { default as useTranslate } from './useTranslate';
