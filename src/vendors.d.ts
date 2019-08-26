4c5354d41c09261d287cf075cf314259948fb0ce
Michael Mok <pmmmwh@gmail.com>
2019-08-27 00:47:18 +0800

    feat(polyglot): augment polyglot type definitions

---
+++ src/vendors.d.ts
@@ -0,0 +1,21 @@
+// Override Type definitions for node-polyglot v2.3.1
+import { InterpolationOptions, PolyglotOptions } from 'node-polyglot';
+
+declare module 'node-polyglot' {
+  interface InterpolationParams {
+    suffix?: string;
+    prefix?: string;
+  }
+
+  interface PolyglotOptions {
+    phrases?: any;
+    locale?: string;
+    allowMissing?: boolean;
+    onMissingKey?: (
+      key: string,
+      options?: InterpolationOptions,
+      locale?: string
+    ) => string;
+    interpolation?: InterpolationParams;
+  }
+}
