56fb06327ef2978a7569a4dd8cad91f8701290b5
Michael Mok <pmmmwh@gmail.com>
2019-08-29 01:22:45 +0800

    test(use-translate): restructure tests for readability

--- src/__tests__/useTranslate.test.tsx
+++ src/__tests__/useTranslate.test.tsx
@@ -17,46 +17,95 @@ describe('Use Translate', () => {
     console.error = originalConsoleError;
   });
 
-  it('should return key and warn without context', () => {
+  it('should return a function', () => {
     const { result } = renderHook(() => useTranslate());
     expect(typeof result.current).toBe('function');
-
-    const callResult = result.current('key');
-    expect(callResult).toBe('key');
-    expect(consoleOutput).toHaveLength(1);
-    expect(consoleOutput[0]).toBe(
-      'Warning: t is called without Polyglot context. Perhaps you need to wrap the component in <I18n>?'
-    );
   });
 
-  it('should return available message with context', () => {
-    const wrapper: React.FC = ({ children }) => (
-      <I18n locale="en" phrases={{ key: 'Message' }}>
-        {children}
-      </I18n>
-    );
-    const { result } = renderHook(() => useTranslate(), { wrapper });
-    expect(typeof result.current).toBe('function');
-
-    const callResult = result.current('key');
-    expect(callResult).toBe('Message');
-    expect(consoleOutput).toHaveLength(0);
+  describe('when context is not provided', () => {
+    it('should return key on call and emit a warning', () => {
+      const { result } = renderHook(() => useTranslate());
+      const callResult = result.current('phrase');
+      expect(callResult).toBe('phrase');
+      expect(consoleOutput).toHaveLength(1);
+      expect(consoleOutput[0]).toBe(
+        'Warning: t is called without Polyglot context. Perhaps you need to wrap the component in <I18n>?'
+      );
+    });
   });
 
-  it('should return key and warn for unavailable message with context', () => {
-    const wrapper: React.FC = ({ children }) => (
-      <I18n locale="en" phrases={{ key: 'Message' }}>
-        {children}
-      </I18n>
-    );
-    const { result } = renderHook(() => useTranslate(), { wrapper });
-    expect(typeof result.current).toBe('function');
+  describe('when context is provided', () => {
+    describe('when the called phrase is not found', () => {
+      it('should return the key and emit a warning', () => {
+        const wrapper: React.FC = ({ children }) => (
+          <I18n locale="en" phrases={{ phrase: 'Message' }}>
+            {children}
+          </I18n>
+        );
+        const { result } = renderHook(() => useTranslate(), { wrapper });
+        const callResult = result.current('unavailable');
+        expect(callResult).toBe('unavailable');
+        expect(consoleOutput).toHaveLength(1);
+        expect(consoleOutput[0]).toBe(
+          'Warning: Missing translation for key: "unavailable"'
+        );
+      });
 
-    const callResult = result.current('unavailable');
-    expect(callResult).toBe('unavailable');
-    expect(consoleOutput).toHaveLength(1);
-    expect(consoleOutput[0]).toBe(
-      'Warning: Missing translation for key: "unavailable"'
-    );
+      it('should not interpolate without a fallback', () => {
+        const wrapper: React.FC = ({ children }) => (
+          <I18n locale="en" phrases={{ phrase: 'Message' }}>
+            {children}
+          </I18n>
+        );
+        const { result } = renderHook(() => useTranslate(), { wrapper });
+        const callResult = result.current('unavailable', {
+          message: 'Failed!',
+        });
+        expect(callResult).not.toContain('Failed!');
+        expect(callResult).toBe('unavailable');
+      });
+
+      it('should interpolate to the fallback', () => {
+        const wrapper: React.FC = ({ children }) => (
+          <I18n locale="en" phrases={{ phrase: 'Message' }}>
+            {children}
+          </I18n>
+        );
+        const { result } = renderHook(() => useTranslate(), { wrapper });
+        const callResult = result.current('unavailable', {
+          _: 'Fallback: %{message}',
+          message: 'Success!',
+        });
+        expect(callResult).not.toContain('unavailable');
+        expect(callResult).toBe('Fallback: Success!');
+      });
+    });
+
+    describe('when the called phrase is found', () => {
+      it('should return the phrase without warning', () => {
+        const wrapper: React.FC = ({ children }) => (
+          <I18n locale="en" phrases={{ phrase: 'Message' }}>
+            {children}
+          </I18n>
+        );
+        const { result } = renderHook(() => useTranslate(), { wrapper });
+        const callResult = result.current('phrase');
+        expect(callResult).toBe('Message');
+        expect(consoleOutput).toHaveLength(0);
+      });
+
+      it('should interpolate to the phrase', () => {
+        const wrapper: React.FC = ({ children }) => (
+          <I18n locale="en" phrases={{ phrase: 'Interpolated: %{message}' }}>
+            {children}
+          </I18n>
+        );
+        const { result } = renderHook(() => useTranslate(), { wrapper });
+        const callResult = result.current('phrase', {
+          message: 'Success!',
+        });
+        expect(callResult).toBe('Interpolated: Success!');
+      });
+    });
   });
 });
