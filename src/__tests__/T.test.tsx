f66ee1a0fda247d7a25fb8e745ff8d55f570d2c8
Michael Mok <pmmmwh@gmail.com>
2019-08-29 01:10:32 +0800

    test(t): structure tests according to use locale

--- src/__tests__/T.test.tsx
+++ src/__tests__/T.test.tsx
@@ -30,101 +30,103 @@ describe('T Component', () => {
     });
   });
 
-  describe('when a phrase is not found', () => {
-    it('should render the key and emit a warning', () => {
-      const tree = (
-        <I18n locale="en" phrases={{ phrase: 'Message' }}>
-          <T phrase="unavailable" />
-        </I18n>
-      );
-      const { getByText, queryByText } = render(tree);
-      expect(queryByText('phrase')).not.toBeInTheDocument();
-      expect(queryByText('Message')).not.toBeInTheDocument();
-      expect(getByText('unavailable')).toBeInTheDocument();
-      expect(consoleOutput).toHaveLength(1);
-      expect(consoleOutput[0]).toBe(
-        'Warning: Missing translation for key: "unavailable"'
-      );
+  describe('when context is provided', () => {
+    describe('when a phrase is not found', () => {
+      it('should render the key and emit a warning', () => {
+        const tree = (
+          <I18n locale="en" phrases={{ phrase: 'Message' }}>
+            <T phrase="unavailable" />
+          </I18n>
+        );
+        const { getByText, queryByText } = render(tree);
+        expect(queryByText('phrase')).not.toBeInTheDocument();
+        expect(queryByText('Message')).not.toBeInTheDocument();
+        expect(getByText('unavailable')).toBeInTheDocument();
+        expect(consoleOutput).toHaveLength(1);
+        expect(consoleOutput[0]).toBe(
+          'Warning: Missing translation for key: "unavailable"'
+        );
+      });
+
+      it('should render the fallback without warning', () => {
+        const tree = (
+          <I18n locale="en" phrases={{ phrase: 'Message' }}>
+            <T phrase="unavailable" fallback="Fallback" />
+          </I18n>
+        );
+        const { getByText, queryByText } = render(tree);
+        expect(queryByText('phrase')).not.toBeInTheDocument();
+        expect(queryByText('Message')).not.toBeInTheDocument();
+        expect(getByText('Fallback')).toBeInTheDocument();
+        expect(consoleOutput).toHaveLength(0);
+      });
+
+      it('should interpolate values to a fallback', () => {
+        const tree = (
+          <I18n locale="en" phrases={{ phrase: 'Interpolated: %{message}' }}>
+            <T
+              phrase="unavailable"
+              fallback="Fallback: %{message}"
+              interpolations={{ message: 'Success!' }}
+            />
+          </I18n>
+        );
+        const { getByText, queryByText } = render(tree);
+        expect(queryByText(/^Interpolated: /)).not.toBeInTheDocument();
+        expect(getByText(/^Fallback: /)).toBeInTheDocument();
+        expect(getByText(/^Fallback: /).textContent).toBe('Fallback: Success!');
+      });
+
+      it('should not interpolate values without a fallback', () => {
+        const tree = (
+          <I18n locale="en" phrases={{ phrase: 'Interpolated: %{message}' }}>
+            <T phrase="unavailable" interpolations={{ message: 'Success!' }} />
+          </I18n>
+        );
+        const { getByText, queryByText } = render(tree);
+        expect(queryByText(/^Interpolated: /)).not.toBeInTheDocument();
+        expect(queryByText(/Success!/)).not.toBeInTheDocument();
+        expect(getByText('unavailable')).toBeInTheDocument();
+        expect(getByText('unavailable').textContent).toBe('unavailable');
+      });
     });
 
-    it('should render the fallback without warning', () => {
-      const tree = (
-        <I18n locale="en" phrases={{ phrase: 'Message' }}>
-          <T phrase="unavailable" fallback="Fallback" />
-        </I18n>
-      );
-      const { getByText, queryByText } = render(tree);
-      expect(queryByText('phrase')).not.toBeInTheDocument();
-      expect(queryByText('Message')).not.toBeInTheDocument();
-      expect(getByText('Fallback')).toBeInTheDocument();
-      expect(consoleOutput).toHaveLength(0);
-    });
+    describe('when a phrase is found', () => {
+      it('should render without warning', () => {
+        const tree = (
+          <I18n locale="en" phrases={{ phrase: 'Message' }}>
+            <T phrase="phrase" />
+          </I18n>
+        );
+        const { getByText } = render(tree);
+        expect(getByText('Message')).toBeInTheDocument();
+        expect(consoleOutput).toHaveLength(0);
+      });
 
-    it('should interpolate values to a fallback', () => {
-      const tree = (
-        <I18n locale="en" phrases={{ phrase: 'Interpolated: %{message}' }}>
-          <T
-            phrase="unavailable"
-            fallback="Fallback: %{message}"
-            interpolations={{ message: 'Success!' }}
-          />
-        </I18n>
-      );
-      const { getByText, queryByText } = render(tree);
-      expect(queryByText(/^Interpolated: /)).not.toBeInTheDocument();
-      expect(getByText(/^Fallback: /)).toBeInTheDocument();
-      expect(getByText(/^Fallback: /).textContent).toBe('Fallback: Success!');
-    });
+      it('should render the phrase even a fallback is provided', () => {
+        const tree = (
+          <I18n locale="en" phrases={{ phrase: 'Message' }}>
+            <T phrase="phrase" fallback="Fallback" />
+          </I18n>
+        );
+        const { getByText, queryByText } = render(tree);
+        expect(queryByText('phrase')).not.toBeInTheDocument();
+        expect(queryByText('Fallback')).not.toBeInTheDocument();
+        expect(getByText('Message')).toBeInTheDocument();
+      });
 
-    it('should not interpolate values without a fallback', () => {
-      const tree = (
-        <I18n locale="en" phrases={{ phrase: 'Interpolated: %{message}' }}>
-          <T phrase="unavailable" interpolations={{ message: 'Success!' }} />
-        </I18n>
-      );
-      const { getByText, queryByText } = render(tree);
-      expect(queryByText(/^Interpolated: /)).not.toBeInTheDocument();
-      expect(queryByText(/Success!/)).not.toBeInTheDocument();
-      expect(getByText('unavailable')).toBeInTheDocument();
-      expect(getByText('unavailable').textContent).toBe('unavailable');
-    });
-  });
-
-  describe('when a phrase is found', () => {
-    it('should render without warning', () => {
-      const tree = (
-        <I18n locale="en" phrases={{ phrase: 'Message' }}>
-          <T phrase="phrase" />
-        </I18n>
-      );
-      const { getByText } = render(tree);
-      expect(getByText('Message')).toBeInTheDocument();
-      expect(consoleOutput).toHaveLength(0);
-    });
-
-    it('should render the phrase even a fallback is provided', () => {
-      const tree = (
-        <I18n locale="en" phrases={{ phrase: 'Message' }}>
-          <T phrase="phrase" fallback="Fallback" />
-        </I18n>
-      );
-      const { getByText, queryByText } = render(tree);
-      expect(queryByText('phrase')).not.toBeInTheDocument();
-      expect(queryByText('Fallback')).not.toBeInTheDocument();
-      expect(getByText('Message')).toBeInTheDocument();
-    });
-
-    it('should interpolate values', () => {
-      const tree = (
-        <I18n locale="en" phrases={{ phrase: 'Interpolated: %{message}' }}>
-          <T phrase="phrase" interpolations={{ message: 'Success!' }} />
-        </I18n>
-      );
-      const { getByText } = render(tree);
-      expect(getByText(/^Interpolated: /)).toBeInTheDocument();
-      expect(getByText(/^Interpolated: /).textContent).toBe(
-        'Interpolated: Success!'
-      );
+      it('should interpolate values', () => {
+        const tree = (
+          <I18n locale="en" phrases={{ phrase: 'Interpolated: %{message}' }}>
+            <T phrase="phrase" interpolations={{ message: 'Success!' }} />
+          </I18n>
+        );
+        const { getByText } = render(tree);
+        expect(getByText(/^Interpolated: /)).toBeInTheDocument();
+        expect(getByText(/^Interpolated: /).textContent).toBe(
+          'Interpolated: Success!'
+        );
+      });
     });
   });
 });
