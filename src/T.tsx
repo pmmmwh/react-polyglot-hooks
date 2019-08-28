f919872190b0c6b8131f549d40e1463dbe1e5777
Michael Mok <pmmmwh@gmail.com>
2019-08-29 00:27:58 +0800

    feat(t): add a fallback prop for easier consumption

--- src/T.tsx
+++ src/T.tsx
@@ -3,15 +3,27 @@ import { InterpolationOptions } from 'node-polyglot';
 import useTranslate from './useTranslate';
 
 export interface TProps {
-  phrase: string;
+  fallback?: string;
   options?: number | InterpolationOptions;
+  phrase: string;
 }
 
-const T: React.FC<TProps> = ({ phrase, options }) => {
+const T: React.FC<TProps> = ({ fallback, options, phrase }) => {
   const t = useTranslate();
+
+  const tOptions = {
+    // Check for a fallback prop before assigning it as it will alter the options
+    ...(fallback ? { _: fallback } : undefined),
+    // Check for existence of the interpolations prop
+    ...(options &&
+      // Handles smart_count from a number
+      // This allows the fallback to be safely assigned
+      (typeof options === 'number' ? { smart_count: options } : options)),
+  };
+
   // HACK: A workaround for the current limitations of TSX with FC
   // Ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20544
-  return (t(phrase, options) as unknown) as React.ReactElement;
+  return (t(phrase, tOptions) as unknown) as React.ReactElement;
 };
 
 export default T;
