b6a8aed7cf5dff429edb403689ac5640dc4505e1
Michael Mok <pmmmwh@gmail.com>
2019-08-29 01:27:51 +0800

    test(use-locale): add check for string type

--- src/__tests__/useLocale.test.tsx
+++ src/__tests__/useLocale.test.tsx
@@ -6,19 +6,19 @@ describe('Use Locale', () => {
   describe('when context is not provided', () => {
     it('should return undefined', () => {
       const { result } = renderHook(() => useLocale());
-
       expect(result.current).toBe(undefined);
     });
   });
 
   describe('when context is provided', () => {
-    it('should return I18n locale', () => {
+    it('should return I18n locale string', () => {
       const wrapper: React.FC = ({ children }) => (
         <I18n locale="en" phrases={{}}>
           {children}
         </I18n>
       );
       const { result } = renderHook(() => useLocale(), { wrapper });
+      expect(typeof result.current).toBe('string');
       expect(result.current).toBe('en');
     });
   });
