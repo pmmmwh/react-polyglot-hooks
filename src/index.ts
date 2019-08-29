e58238023f36f8a7691d887b133eff442bcd092b
Michael Mok <pmmmwh@gmail.com>
2019-08-30 06:02:16 +0800

    feat: export PolyglotT type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { PolyglotT } from './constants';
 export { default as I18n, I18nProps } from './I18n';
 export { I18nContextProps } from './i18nContext';
 export { default as T, TProps } from './T';
