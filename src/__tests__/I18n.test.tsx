92046f5c0eec5d4d0afa8f94430b2b3fb9ab9808
Michael Mok <pmmmwh@gmail.com>
2019-08-30 02:52:50 +0800

    test(i18n): update tests to reflect allowance of multiple children

--- src/__tests__/I18n.test.tsx
+++ src/__tests__/I18n.test.tsx
@@ -7,132 +7,99 @@ import I18n, { I18nProps } from '../I18n';
 import I18nContext from '../i18nContext';
 
 describe('I18n Provider', () => {
-  const originalConsoleError = console.error;
-
-  let consoleOutput: string[] = [];
-  beforeEach(() => {
-    console.error = (...args: string[]): void => {
-      args.forEach(arg => consoleOutput.push(arg));
-    };
+  it('should render without crashing', () => {
+    render(<I18n locale="" phrases={{}} />);
   });
 
-  afterEach(() => {
-    consoleOutput = [];
-    console.error = originalConsoleError;
+  it('should only pass locale and t to children via context', () => {
+    const readValue = spy();
+    const tree = (
+      <I18n locale="en" phrases={{}}>
+        <I18nContext.Consumer>
+          {values => <ValueTester callback={readValue} value={values} />}
+        </I18nContext.Consumer>
+      </I18n>
+    );
+    render(tree);
+    const calledValue = readValue.getCall(0).args[0];
+    expect(typeof calledValue).toBe('object');
+
+    const valueKeys = Object.keys(calledValue);
+    expect(valueKeys).toHaveLength(2);
+    expect(valueKeys).toStrictEqual(['locale', 't']);
   });
 
-  describe('when no children is provided', () => {
-    it('should throw an invariant error', () => {
-      expect(() => render(<I18n locale="" phrases={{}} />)).toThrow();
-      expect(consoleOutput).toHaveLength(3);
-      expect(consoleOutput[0]).toContain('Invariant Violation');
-    });
+  it('should provide a locale from Polyglot', () => {
+    const tree = (
+      <I18n locale="en" phrases={{}}>
+        <I18nContext.Consumer>
+          {({ locale }) => <span>Received: {locale}</span>}
+        </I18nContext.Consumer>
+      </I18n>
+    );
+    const { getByText } = render(tree);
+    expect(getByText(/^Received:/).textContent).toBe('Received: en');
   });
 
-  describe('when multiple children is provided', () => {
-    it('should throw an invariant error', () => {
-      expect(() =>
-        render(
-          <I18n locale="" phrases={{}}>
-            <div />
-            <div />
-          </I18n>
-        )
-      ).toThrow();
-      expect(consoleOutput).toHaveLength(3);
-      expect(consoleOutput[0]).toContain('Invariant Violation');
-    });
+  it('should provide default locale (en) from Polyglot', () => {
+    const tree = (
+      <I18n locale="" phrases={{}}>
+        <I18nContext.Consumer>
+          {({ locale }) => <span>Received: {locale}</span>}
+        </I18nContext.Consumer>
+      </I18n>
+    );
+    const { getByText } = render(tree);
+    expect(getByText(/^Received:/).textContent).toBe('Received: en');
   });
 
-  describe('when a single child is provided', () => {
-    it('should render without crashing', () => {
-      const { getByText } = render(
-        <I18n locale="" phrases={{}}>
-          <span>Children</span>
-        </I18n>
-      );
-      expect(getByText('Children')).toBeInTheDocument();
-      expect(consoleOutput).toHaveLength(0);
-    });
+  it('should provide a working t function from Polyglot', () => {
+    const readValue = spy();
+    const tree = (
+      <I18n locale="en" phrases={{ phrase: 'Message' }}>
+        <I18nContext.Consumer>
+          {({ t }) => <ValueTester callback={readValue} value={t} />}
+        </I18nContext.Consumer>
+      </I18n>
+    );
+    render(tree);
+    const calledValue = readValue.getCall(0).args[0];
+    expect(typeof calledValue).toBe('function');
+    expect(calledValue('phrase')).toBe('Message');
+  });
 
-    it('should only pass locale and t to children via context', () => {
-      const readValue = spy();
-      const tree = (
-        <I18n locale="en" phrases={{}}>
-          <I18nContext.Consumer>
-            {values => <ValueTester callback={readValue} value={values} />}
-          </I18nContext.Consumer>
-        </I18n>
-      );
-      render(tree);
-      const calledValue = readValue.getCall(0).args[0];
-      expect(typeof calledValue).toBe('object');
+  it('should update after a change in locale', () => {
+    const Tree: React.FC<Pick<I18nProps, 'locale'>> = ({ locale }) => (
+      <I18n locale={locale} phrases={{}}>
+        <I18nContext.Consumer>
+          {({ locale }) => <span>Received: {locale}</span>}
+        </I18nContext.Consumer>
+      </I18n>
+    );
+    const { getByText, rerender } = render(<Tree locale="en" />);
+    expect(getByText(/^Received:/).textContent).toBe('Received: en');
 
-      const valueKeys = Object.keys(calledValue);
-      expect(valueKeys).toHaveLength(2);
-      expect(valueKeys).toStrictEqual(['locale', 't']);
-    });
+    rerender(<Tree locale="zh" />);
+    expect(getByText(/^Received:/).textContent).toBe('Received: zh');
+  });
 
-    it('should provide a locale from Polyglot', () => {
-      const tree = (
-        <I18n locale="en" phrases={{}}>
-          <I18nContext.Consumer>
-            {({ locale }) => <span>Received: {locale}</span>}
-          </I18nContext.Consumer>
-        </I18n>
-      );
-      const { getByText } = render(tree);
-      expect(getByText(/^Received:/).textContent).toBe('Received: en');
-    });
+  it('should update after a change in phrases', () => {
+    const readValue = spy();
+    const Tree: React.FC<Pick<I18nProps, 'phrases'>> = ({ phrases }) => (
+      <I18n locale="en" phrases={phrases}>
+        <I18nContext.Consumer>
+          {({ t }) => <ValueTester callback={readValue} value={t} />}
+        </I18nContext.Consumer>
+      </I18n>
+    );
+    const { rerender } = render(<Tree phrases={{ test: 'Test' }} />);
+    const firstValue = readValue.getCall(0).args[0];
+    expect(typeof firstValue).toBe('function');
+    expect(firstValue('test')).toBe('Test');
 
-    it('should provide a working t function from Polyglot', () => {
-      const readValue = spy();
-      const tree = (
-        <I18n locale="en" phrases={{ phrase: 'Message' }}>
-          <I18nContext.Consumer>
-            {({ t }) => <ValueTester callback={readValue} value={t} />}
-          </I18nContext.Consumer>
-        </I18n>
-      );
-      render(tree);
-      const calledValue = readValue.getCall(0).args[0];
-      expect(typeof calledValue).toBe('function');
-      expect(calledValue('phrase')).toBe('Message');
-    });
-
-    it('should update after a change in locale', () => {
-      const Tree: React.FC<Pick<I18nProps, 'locale'>> = ({ locale }) => (
-        <I18n locale={locale} phrases={{}}>
-          <I18nContext.Consumer>
-            {({ locale }) => <span>Received: {locale}</span>}
-          </I18nContext.Consumer>
-        </I18n>
-      );
-      const { getByText, rerender } = render(<Tree locale="en" />);
-      expect(getByText(/^Received:/).textContent).toBe('Received: en');
-
-      rerender(<Tree locale="zh" />);
-      expect(getByText(/^Received:/).textContent).toBe('Received: zh');
-    });
-
-    it('should update after a change in phrases', () => {
-      const readValue = spy();
-      const Tree: React.FC<Pick<I18nProps, 'phrases'>> = ({ phrases }) => (
-        <I18n locale="en" phrases={phrases}>
-          <I18nContext.Consumer>
-            {({ t }) => <ValueTester callback={readValue} value={t} />}
-          </I18nContext.Consumer>
-        </I18n>
-      );
-      const { rerender } = render(<Tree phrases={{ test: 'Test' }} />);
-      const firstValue = readValue.getCall(0).args[0];
-      expect(typeof firstValue).toBe('function');
-      expect(firstValue('test')).toBe('Test');
-
-      rerender(<Tree phrases={{ test: 'Expected' }} />);
-      const secondValue = readValue.getCall(1).args[0];
-      expect(typeof secondValue).toBe('function');
-      expect(secondValue('test')).toBe('Expected');
-    });
+    rerender(<Tree phrases={{ test: 'Expected' }} />);
+    const secondValue = readValue.getCall(1).args[0];
+    expect(typeof secondValue).toBe('function');
+    expect(secondValue('test')).toBe('Expected');
   });
 });
