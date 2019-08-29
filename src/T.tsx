05241257536d0976fa0bcef7f04d3a75549f3ce3
Michael Mok <pmmmwh@gmail.com>
2019-08-30 00:52:34 +0800

    feat(t): add a count prop as a shorthand to use smart_count

--- src/T.tsx
+++ src/T.tsx
@@ -3,24 +3,38 @@ import { InterpolationOptions } from 'node-polyglot';
 import useTranslate from './useTranslate';
 
 export interface TProps {
+  count?: number;
   fallback?: string;
   interpolations?: number | InterpolationOptions;
   phrase: string;
 }
 
-const T: React.FC<TProps> = ({ fallback, interpolations, phrase }) => {
+const T: React.FC<TProps> = ({ count, fallback, interpolations, phrase }) => {
   const t = useTranslate();
 
+  let cleanInterpolations;
+  if (typeof interpolations !== 'number') {
+    cleanInterpolations = interpolations;
+  } else {
+    // TODO: Deprecate number from interpolations in v0.3.0
+    // Handles number interpolation
+    cleanInterpolations = { smart_count: interpolations };
+    if (process.env.NODE_ENV !== 'production') {
+      console.warn(
+        [
+          'Use of the interpolations prop as a shorthand for smart_count have been deprecated in favor of the count prop and will be removed in the next major version.',
+          'Please update your app to use that instead.',
+        ].join(' ')
+      );
+    }
+  }
+
   const tOptions = {
-    // Check for a fallback prop before assigning it as it will alter the options
-    ...(fallback ? { _: fallback } : undefined),
-    // Check for existence of the interpolations prop
-    ...(interpolations &&
-      // Handles smart_count from a number
-      // This allows the fallback to be safely assigned
-      (typeof interpolations === 'number'
-        ? { smart_count: interpolations }
-        : interpolations)),
+    // Check for truthy prop values before assigning
+    // This is done to prevent altering the specific options
+    ...(fallback && { _: fallback }),
+    ...(count && { smart_count: count }),
+    ...cleanInterpolations,
   };
 
   // HACK: A workaround for the current limitations of TSX with FC
