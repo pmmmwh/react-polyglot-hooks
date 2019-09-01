e61327f0b49c3bda82d08ce0b8a8d43d678ddcce
Michael Mok <pmmmwh@gmail.com>
2019-09-02 04:19:34 +0800

    refactor(use-t): rename useTranslate to useT

--- src/useT.ts
+++ src/useT.ts
@@ -2,9 +2,9 @@ import { useContext } from 'react';
 import { PolyglotT } from './constants';
 import I18nContext from './i18nContext';
 
-const useTranslate = (): PolyglotT => {
+const useT = (): PolyglotT => {
   const { t } = useContext(I18nContext);
   return t;
 };
 
-export default useTranslate;
+export default useT;
