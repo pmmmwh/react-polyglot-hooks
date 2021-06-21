63a46ad9a8fc3cfff4f9c63a4746f6ece9a8ece8
Michael Mok <pmmmwh@gmail.com>
2021-06-21 13:12:05 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{js,ts,tsx}', '!src/**/*.d.ts'],
   coverageDirectory: 'coverage',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  testEnvironment: 'jsdon',
+  testEnvironment: 'jsdom',
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,ts,tsx}',
     '<rootDir>/src/**/*.{spec,test}.{js,ts,tsx}',
