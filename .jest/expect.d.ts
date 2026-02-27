5c50d7d966a2cfd1a8578d0bb4ac6339164e8a02
Michael Mok <pmmmwh@gmail.com>
2026-02-27 14:07:22 +0100

    chore: fix prettier issues

--- .jest/expect.d.ts
+++ .jest/expect.d.ts
@@ -2,6 +2,8 @@ import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 
 declare module 'expect' {
   interface AsymmetricMatchers extends TestingLibraryMatchers<(str: string) => any, void> {}
-  interface Matchers<R extends void | Promise<void>, T = unknown>
-    extends TestingLibraryMatchers<(str: string) => any, R> {}
+  interface Matchers<R extends void | Promise<void>, T = unknown> extends TestingLibraryMatchers<
+    (str: string) => any,
+    R
+  > {}
 }
